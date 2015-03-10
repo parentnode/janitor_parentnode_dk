fc99f1002cbfdd3f89b4daadf289130e7c27579e
kaestel <kaestel@e-types.com>
2015-03-10 19:39:40 +0000

    documentation

--- src/www/getting-started.php
+++ src/www/getting-started.php
@@ -10,25 +10,12 @@ include_once($_SERVER["FRAMEWORK_PATH"]."/config/init.php");
 $action = $page->actions();
 
 
-$page->bodyClass("docs");
+$page->bodyClass("gettingstarted");
 $page->pageTitle("Everyone deserves a good start");
 
 
-if(is_array($action) && count($action)) {
-
-	if(count($action) == 1) {
-
-		# /getting-started/#sindex#
-		$page->page(array(
-			"templates" => "getting-started/".$action[0].".php"
-		));
-		exit();
-	}
-
-}
-
 $page->page(array(
-	"templates" => "getting-started/index.php"
+	"templates" => "pages/getting-started.php"
 ));
 exit();
 
