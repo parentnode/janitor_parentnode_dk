a92a47d9a630b7eef86558d72ed7812fb28e345f
kaestel <martin@think.dk>
2015-12-27 18:04:26 +0100

    Added nested navigation

--- src/templates/janitor.footer.php
+++ src/templates/janitor.footer.php
@@ -1,27 +1,37 @@
 	</div>
 
 	<div id="navigation">
-		<ul>
-
-			<?= $HTML->link("Tests", "/janitor/tests", array("wrapper" => "li.test")) ?>
-
-
-			<?= $HTML->link("Pages", "/janitor/admin/page/list", array("wrapper" => "li.page")) ?>
-			<?= $HTML->link("Todos", "/janitor/admin/todo/list", array("wrapper" => "li.todo")) ?>
-
-			<?= $HTML->link("Navigations", "/janitor/admin/navigation/list", array("wrapper" => "li.navigation")) ?>
-
-			<?= $HTML->link("Tags", "/janitor/admin/tag/list", array("wrapper" => "li.tags")) ?>
-			<?= $HTML->link("Users", "/janitor/admin/user/list", array("wrapper" => "li.user")) ?>
-
-			<?= $HTML->link("Profile", "/janitor/admin/profile", array("wrapper" => "li.profile")) ?>
-			<?= $HTML->link("Log", "/janitor/admin/log/list", array("wrapper" => "li.logs")) ?>
+		<ul class="sections">
+			<li class="content">
+				<h3>Content</h3>
+				<ul>
+					<?= $HTML->link("Tests", "/janitor/tests", array("wrapper" => "li.test")) ?>
+					<?= $HTML->link("Pages", "/janitor/admin/page/list", array("wrapper" => "li.page")) ?>
+					<?= $HTML->link("Todos", "/janitor/admin/todo/list/state/30", array("wrapper" => "li.todo")) ?>
+					<?= $HTML->link("Wishes", "/janitor/admin/wish/list", array("wrapper" => "li.wish")) ?>
+				</ul>
+			</li>
+			<li class="site">
+				<h3>Site</h3>
+				<ul>
+					<?= $HTML->link("Navigations", "/janitor/admin/navigation/list", array("wrapper" => "li.navigation")) ?>
+					<?= $HTML->link("Tags", "/janitor/admin/tag/list", array("wrapper" => "li.tags")) ?>
+					<?= $HTML->link("Log", "/janitor/admin/log/list", array("wrapper" => "li.logs")) ?>
+				</ul>
+			</li>
+			<li class="users">
+				<h3>Users</h3>
+				<ul>
+					<?= $HTML->link("Users", "/janitor/admin/user/list", array("wrapper" => "li.user")) ?>
+					<?= $HTML->link("Profile", "/janitor/admin/profile", array("wrapper" => "li.profile")) ?>
+				</ul>
+			</li>
 		</ul>
 	</div>
 
 	<div id="footer">
 		<ul class="servicenavigation">
-			<li class="copyright">Janitor, Manipulator, Modulator - parentNode - Copyright 2015</li>
+			<li class="copyright">Janitor, Manipulator, Modulator - parentNode - Copyright 2016</li>
 		</ul>
 	</div>
 </div>
