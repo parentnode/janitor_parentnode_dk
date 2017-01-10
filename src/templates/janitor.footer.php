26cf1bdcfc762044ff3be44f8e1b9f302e3d5a12
kaestel <martin@think.dk>
2017-01-11 00:32:49 +0100

    Global alignment

--- src/templates/janitor.footer.php
+++ src/templates/janitor.footer.php
@@ -16,12 +16,14 @@
 					<?= $HTML->link("Navigations", "/janitor/admin/navigation/list", array("wrapper" => "li.navigation")) ?>
 					<?= $HTML->link("Tags", "/janitor/admin/tag/list", array("wrapper" => "li.tags")) ?>
 					<?= $HTML->link("Log", "/janitor/admin/log/list", array("wrapper" => "li.logs")) ?>
+					<?= $HTML->link("Cache", "/janitor/admin/system/cache", array("wrapper" => "li.cache")) ?>
 				</ul>
 			</li>
 			<li class="users">
 				<h3>Users</h3>
 				<ul class="subjects">
 					<?= $HTML->link("Users", "/janitor/admin/user/list", array("wrapper" => "li.user")) ?>
+					<?= $HTML->link("Groups", "/janitor/admin/user/group/list", array("wrapper" => "li.usergroup")) ?>
 					<?= $HTML->link("Profile", "/janitor/admin/profile", array("wrapper" => "li.profile")) ?>
 				</ul>
 			</li>
