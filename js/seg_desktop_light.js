9835bbd5d00e6f1dd61fa3583e97eec61e701f49
threepwoodd <scope.dk@gmail.com>
2019-03-26 16:00:27 +0100

    Squashed 'theme/www/assets/parentnode-skin-default/' changes from d1f39f4b..cb733ad6
    cb733ad6 build
    ecfcacaa Smartphone ajax signup
    
    git-subtree-dir: theme/www/assets/parentnode-skin-default
    git-subtree-split: cb733ad6efb777558f492c3e5d0544f2294f9e2e

--- js/seg_desktop_light.js
+++ js/seg_desktop_light.js
@@ -1,6 +1,6 @@
 /*
 Manipulator v0.9.2-full Copyright 2017 http://manipulator.parentnode.dk
-asset-builder @ 2019-03-25 17:25:51
+asset-builder @ 2019-03-26 15:51:35
 */
 
 /*seg_desktop_light_include.js*/
