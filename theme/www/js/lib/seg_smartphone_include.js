e7510328f89b2fd644fbea1f67b10a202680dc84
Martin Kæstel Nielsen <martin@think.dk>
2019-10-12 22:12:35 +0200

    Update for skin

--- theme/www/js/lib/seg_smartphone_include.js
+++ theme/www/js/lib/seg_smartphone_include.js
@@ -5,7 +5,6 @@ document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></scr
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/manipulator/src/u-googleanalytics.js"></script>');
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/manipulator/src/u-form-builder.js"></script>');
 
-document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/smartphone/i-login.js"></script>');
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/smartphone/i-signup.js"></script>');
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/smartphone/i-verify.js"></script>');
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/smartphone/i-documentation.js"></script>');
