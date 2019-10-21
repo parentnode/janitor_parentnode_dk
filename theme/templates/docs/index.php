e01600582650fb0a206b5bde88af5f74b131a7b8
Søren Lyhne <lyhnesoren@gmail.com>
2019-10-21 16:26:49 +0200

    update docs index with categories

--- theme/templates/docs/index.php
+++ theme/templates/docs/index.php
@@ -48,73 +48,6 @@ $page_item = $IC->getItem(array("tags" => "page:documentation", "extend" => arra
 	<div class="files" id="library_files">
 
 		<h2>Index</h2>
-		<ul class="library">
-			<li>
-				<h3><a href="/docs/class-page">Page</a></h3>
-				<p>Core request and response structure</p>
-			</li>
-			<li>
-				<h3><a href="/docs/class-items">Items</a></h3>
-				<p>Querying items and item data</p>
-			</li>
-			<li>
-				<h3><a href="/docs/class-itemtype">Itemtype</a></h3>
-				<p>Creating and manipulating itemtypes</p>
-			</li>
-			<li>
-				<h3><a href="/docs/class-user">Users</a></h3>
-				<p>Plain user creation and manipulation</p>
-			</li>
-			<li>
-				<h3><a href="/docs/class-superuser">Superusers</a></h3>
-				<p>Getting, creating and manipulating users</p>
-			</li>
-			<li>
-				<h3><a href="/docs/class-member">Members</a></h3>
-				<p>Plain member creation and manipulation</p>
-			</li>
-			<li>
-				<h3><a href="/docs/class-supermember">SuperMembers</a></h3>
-				<p>Getting, creating and manipulating members</p>
-			</li>
-			<li>
-				<h3><a href="/docs/class-html">HTML</a></h3>
-				<p>HTML class</p>
-			</li>
-			<li>
-				<h3><a href="/docs/class-html-janitor">Janitor HTML (JML)</a></h3>
-				<p>Janitor style HTML class</p>
-			</li>
-			<!--li>
-				<h3><a href="/docs/class-image">Image</a></h3>
-				<p>Image class</p>
-			</li>
-			<li>
-				<h3><a href="/docs/class-video">Video</a></h3>
-				<p>Video class</p>
-			</li>
-			<li>
-				<h3><a href="/docs/class-audio">Audio</a></h3>
-				<p>Audio class</p>
-			</li-->
-			<li>
-				<h3><a href="/docs/class-dom">DOM</a></h3>
-				<p>DOM for Janitor</p>
-			</li>
-			
-			<li>
-				<h3><a href="/docs/class-session">Session</a></h3>
-				<p>Session helper class</p>
-			</li>
-			<li>
-				<h3><a href="/docs/class-message">Message</a></h3>
-				<p>Message helper class</p>
-			</li>
-			<li>
-				<h3><a href="/docs/class-zipper">Zipper</a></h3>
-				<p>Zip helper class</p>
-			</li>
-		</ul>
 		
 		<h3>Item classes</h3>
 		<ul class="library item">
@@ -139,6 +72,19 @@ $page_item = $IC->getItem(array("tags" => "page:documentation", "extend" => arra
 				<p>Creating, querying and manipulating orders and cart items data.</p>
 			</li>
 		</ul>
+
+		<h3>Subscription classes</h3>
+		<ul class="library subscription">
+			<li>
+				<h3><a href="/docs/class-subscription">Subscription</a></h3>
+				<p>Creating, querying and manipulating subscriptions.</p>
+			</li>
+			<li>
+				<h3><a href="/docs/class-supersubscription">SuperSubscription</a></h3>
+				<p>Creating, querying and manipulating subscriptions.</p>
+			</li>
+		</ul>
+
 		<h3>System classes</h3>
 		<ul class="library system">
 			<li>
@@ -150,6 +96,10 @@ $page_item = $IC->getItem(array("tags" => "page:documentation", "extend" => arra
 				<p>HTML class</p>
 			</li>
 			<li>
+				<h3><a href="/docs/class-html-janitor">Janitor HTML (JML)</a></h3>
+				<p>Janitor style HTML class</p>
+			</li>
+			<li>
 				<h3><a href="/docs/class-model">Model</a></h3>
 				<p>Core janitor model class</p>
 			</li>
@@ -157,18 +107,64 @@ $page_item = $IC->getItem(array("tags" => "page:documentation", "extend" => arra
 				<h3><a href="/docs/class-output">Output</a></h3>
 				<p>Output helper class</p>
 			</li>
+			<li>
+				<h3><a href="/docs/class-session">Session</a></h3>
+				<p>Session helper class</p>
+			</li>
+			<li>
+				<h3><a href="/docs/class-message">Message</a></h3>
+				<p>Message helper class</p>
+			</li>
 		</ul>
 
 		<h3>User classes</h3>
 		<ul class="library user">
 			
 			<li>
+				<h3><a href="/docs/class-user">Users</a></h3>
+				<p>Plain user creation and manipulation</p>
+			</li>
+			<li>
 				<h3><a href="/docs/class-superuser">Superusers</a></h3>
 				<p>Getting, creating and manipulating users</p>
 			</li>
+		</ul>
+		
+		<h3>Member classes</h3>
+		<ul class="library member">
+			
 			<li>
-				<h3><a href="/docs/class-user">Users</a></h3>
-				<p>Plain user creation and manipulation</p>
+				<h3><a href="/docs/class-member">Members</a></h3>
+				<p>Plain member creation and manipulation</p>
+			</li>
+			<li>
+				<h3><a href="/docs/class-supermember">Supermembers</a></h3>
+				<p>Getting, creating and manipulating members</p>
+			</li>
+		</ul>
+
+		<h3>Helper classes</h3>
+		<ul class="library helper">
+			
+			<!--li>
+				<h3><a href="/docs/class-image">Image</a></h3>
+				<p>Image class</p>
+			</li>
+			<li>
+				<h3><a href="/docs/class-video">Video</a></h3>
+				<p>Video class</p>
+			</li>
+			<li>
+				<h3><a href="/docs/class-audio">Audio</a></h3>
+				<p>Audio class</p>
+			</li-->
+			<li>
+				<h3><a href="/docs/class-dom">DOM</a></h3>
+				<p>DOM for Janitor</p>
+			</li>
+			<li>
+				<h3><a href="/docs/class-zipper">Zipper</a></h3>
+				<p>Zip helper class</p>
 			</li>
 		</ul>
 
