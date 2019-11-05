fe07bd181eace91969a967d8dd88d112414439ca
Martin Kæstel Nielsen <martin@think.dk>
2019-11-05 21:10:22 +0100

    Add canonical

--- theme/templates/www.header.php
+++ theme/templates/www.header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="<?= $this->language() ?>">
 <head>
-	<!-- (c) & (p) think.dk 2002-2018 -->
+	<!-- (c) & (p) think.dk 2002-2019 -->
 	<!-- For detailed copyright license, see /terms -->
 	<!-- If you want to use or contribute to this code, visit https://parentnode.dk -->
 	<title><?= $this->pageTitle() ?></title>
@@ -14,8 +14,9 @@
 
 	<?= $this->sharingMetaData() ?>
 
-	<link rel="apple-touch-icon" href="/touchicon.png">
-	<link rel="icon" href="/favicon.png">
+	<link rel="canonical" href="<?= SITE_URL . $this->url ?>" />
+	<link rel="apple-touch-icon" href="/touchicon.png" />
+	<link rel="icon" href="/favicon.png" />
 
 <? if(session()->value("dev")) { ?>
 	<link type="text/css" rel="stylesheet" media="all" href="/css/lib/seg_<?= $this->segment() ?>_include.css" />
