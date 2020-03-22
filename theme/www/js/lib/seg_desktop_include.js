335776e0d0c302272be1c97c58fb8c091fa4f86a
Martin Kæstel Nielsen <martin@think.dk>
2020-03-22 21:27:28 +0100

    Update for manipulator

--- theme/www/js/lib/seg_desktop_include.js
+++ theme/www/js/lib/seg_desktop_include.js
@@ -1,15 +1,15 @@
 // Skin
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/seg_desktop.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></script>');
 
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/manipulator/src/u-googleanalytics.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></script>');
 
-document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/i-signup.js"></script>');
-document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/i-verify.js"></script>');
-document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/i-documentation.js"></script>');
-document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/i-comments.js"></script>');
-document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/u-sharing.js"></script>');
-document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop/u-expandarrow.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/optionals/lib/desktop/m-signup.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/optionals/lib/desktop/m-verify.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/optionals/lib/desktop/m-documentation.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/optionals/lib/desktop/m-comments.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/optionals/lib/desktop/u-sharing.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/optionals/lib/desktop/u-expandarrow.js"></script>');
 
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/manipulator/src/u-form-builder.js"></script>');
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/manipulator/src/beta-u-animation-to.js"></script>');
--- theme/www/js/lib/seg_tablet_light_include.js
+++ theme/www/js/lib/seg_tablet_light_include.js
@@ -2,7 +2,7 @@
 document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/seg_tablet_light.js"></script>');
 document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></script>');
 
-document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop_light/i-documentation.js"></script>');
-document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/lib/desktop_light/i-signup.js"></script>'); 
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/optionals/lib/desktop_light/m-documentation.js"></script>');
+document.write('<script type="text/javascript" src="/assets/parentnode-skin-default/js/optionals/lib/desktop_light/m-signup.js"></script>'); 
 
 // Project
