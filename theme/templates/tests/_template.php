68e2c1d099ad6c6b354c55e519c881c3ac034fab
kaestel <martin@think.dk>
2022-12-22 20:31:40 +0100

    cleanup

--- theme/templates/tests/_template.php
+++ theme/templates/tests/_template.php
@@ -12,28 +12,6 @@
 	<div class="tests #method#">
 		<h3>#Test name#</h3>
 		<? 
-
-		// Your test code
-		if("Your test condition"): ?>
-		<div class="testpassed">#Class::method# - correct</div>
-		<? else: ?>
-		<div class="testfailed">#Class::method# - error</div>
-		<? endif; ?>
-	</div>
-
-</div>
-
-
-<div class="scene i:scene tests">
-	<h1>#What#</h1>	
-	<h2>#what does it do#</h2>
-	<ul class="actions">
-		<?= $HTML->link("Back", "/janitor/tests", array("class" => "button", "wrapper" => "li.back")) ?>
-	</ul>
-
-	<div class="tests #method#">
-		<h3>#Test name#</h3>
-		<? 
 		if(1 && "#method# – #test conditions# – #expected result#") {
 
 			(function() {
